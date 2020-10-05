9454b51df2e67c74b5663604784a6b29cbf911a9
Tommaso Pedroni <pedronitom1@gmail.com>
2020-10-05 21:01:11 +0200

    Fix some things

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-const express = require('expressjs')
-const codes = require('codes.json')
+const express = require('express')
+const bodyParser = require('body-parser')
+const codes = require('./codes.json')
+const fs = require('fs')
 const app = express()
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 
 app.post('/checkCode', (req, res) =>  {
     try{
-        if(req.body.toString() in codes){
-            codes.splice(codes.indexOf(req.body.toString()),1)
+        if(codes.codes.includes(req.body.code)){
+            let c = codes
+            c.codes.splice(c.codes.indexOf(req.body.code),1)
+            fs.writeFile('./codes.json', JSON.stringify(c), 'utf-8', function(err) {
+                if (err) throw err
+                console.log('Done!')
+            })
             res.status(200).send("OK")
         } else {
             res.status(401).send("Unauthorized")
@@ -18,12 +28,22 @@ app.post('/checkCode', (req, res) =>  {
 app.post('/insertCode', (req, res) => {
     try{
         if(req.body.admin === codes.admin){
-            codes.codes.push(req.body.code)
+            let c = codes
+            c.codes.push(req.body.code)
+            fs.writeFile('./codes.json', JSON.stringify(c), 'utf-8', function(err) {
+                if (err) throw err
+                console.log('Done!')
+            })
             res.status(200).send("OK")
         } else {
             res.status(401).send("Unauthorized")
         }
     } catch (err) {
         res.status(500).send(err.text)
+        throw err
     }
+})
+
+app.listen(8000, () => {
+    console.log(`Example app listening at http://localhost:8000`)
 })
